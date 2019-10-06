001bf65a03d08f1cdae46b11a3d13a13157fb2c9
GENZITSU <kyojinsyo@gmail.com>
2019-10-06 09:33:48 +0900

    add setting environment file

---
+++ bin/set_environ.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+### Set up training environment.
+# source data path
+export DATA_PATH = "<your data_path>"
+# mid output directory
+export PREPROCESSED_DATA_PATH = "/home/jovyan/DW2V/preprocessed_data/"
+# final output directory
+export DW2V_PATH = "/home/jovyan/DW2V/"
+# param path for DW2V
+export PARAM_PATH = "/home/jovyan/DW2V/DynamicWordEmbedding/params/DW2V/"
+# api for slack notification
+export SLACK_URL = "<your slack web hock url>"
+
+export NJOB = "10"
+export WORD_FREQ_MIN = "5"
